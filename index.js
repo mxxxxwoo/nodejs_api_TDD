325a873259382d35d3ee0e0b0c5f030c86e0d1c2
mengkko <mengkko@Myung-wooui-MacBook-Pro.local>
2019-05-22 13:33:47 +0900

    first api

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan')
+var users = [
+    {id: 1, name: 'alice'},
+    {id: 2, name: 'bek'},
+    {id: 3, name: 'chris'},
+]
+
+app.use(morgan('dev'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/users', function (req, res) {
+    res.json(users)
 });
 
 app.listen(3000, function () {
