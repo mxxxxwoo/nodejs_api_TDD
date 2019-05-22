1f7e8886039556c54ba8c908960e1625b75dd1d8
mengkko <mengkko@Myung-wooui-MacBook-Pro.local>
2019-05-22 14:07:14 +0900

    TDD master

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 var morgan = require('morgan')
 var users = [
     {id: 1, name: 'alice'},
     {id: 2, name: 'bek'},
-    {id: 3, name: 'chris'},
+    {id: 3, name: 'chris'}
 ]
 
 app.use(morgan('dev'))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-});
+})
 
 app.get('/users', function (req, res) {
     res.json(users)
-});
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
+})
