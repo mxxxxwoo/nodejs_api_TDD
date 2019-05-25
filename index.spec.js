ebefbd210d96b9b04fe4bf560030099571110c17
mengkko <mengkko@Myung-wooui-MacBook-Pro.local>
2019-05-25 22:08:13 +0900

    post user error

--- index.spec.js
+++ index.spec.js
@@ -99,4 +99,20 @@ describe('POST /users', () => {
             body.should.have.property('name', name)
         })
     })
+    describe('실패시', () => {
+        it('name 파라메터 누락시 400을 반환한다', done => {
+            request(app)
+                .post('/users')
+                .send({})
+                .expect(400)
+                .end(done)
+        })
+        it('name이 중복일 경우 409를 반환한다.', done => {
+            request(app)
+                .post('/users')
+                .send({name: 'daniel'})
+                .expect(409)
+                .end(done)
+        })
+    })
 })
